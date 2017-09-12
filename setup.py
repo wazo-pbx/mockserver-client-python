5e89ee66bddc5ddce73217e0b420fe00dd0600ce
Sebastien Duthil <sduthil@wazo.community>
2017-09-12 15:09:58 -0400

    bump version for bugfix
    reason: allows upgrades via pip

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="mockserver-client",
-    version="0.0.1",
+    version="0.0.2",
     packages=setuptools.find_packages(),
     author='Job Evers-Meltzer',
     author_email='jobevers@gmail.com',
